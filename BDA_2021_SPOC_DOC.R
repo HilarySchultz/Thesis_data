6d755600bf9f627a5ed51aade488f619e7bee7d6
Hilary Schultz <hilary1.schultz@umconnect.umt.edu>
2022-03-16 13:40:02 -0600

    Better models and plots

--- BDA_2021_SPOC_DOC.R
+++ BDA_2021_SPOC_DOC.R
@@ -80,6 +80,8 @@ DOC_data %>% ggplot(aes(Conc_ppm)) +
 
 #### DOC GLMMs ####
 # nAGQ = 0 is a less exact form of parameter estimation for GLMMs (helps model run)
+
+# Replicate = random
 docmodel <- glmer(Conc_ppm ~ Date + Reach + (1|Site) + (1|Replicate),
                   data = DOC_data, 
                   family = Gamma(link = "log"))
@@ -89,6 +91,7 @@ AIC(docmodel) # 386.9168
 AICc(docmodel) # 389.4272
 
 # This is the current working model 3/15/22
+# Replicate = NULL
 docmodel1 <- glmer(Conc_ppm ~ Date + Reach + (1|Site),
                   data = DOC_data, 
                   family = Gamma(link = "log"))
@@ -98,70 +101,48 @@ AIC(docmodel1) # 385.1746
 AICc(docmodel1) # 387.3116
 summary(docmodel1)
 
-docmodel2 <- glmer(Conc_ppm ~ Date + Reach + Site + (1|Replicate),
-                   data = DOC_data,
-                   nAGQ = 0,
-                   family = Gamma(link = "log"))
-plot(docmodel2)
-Anova(docmodel2)
-AIC(docmodel2) # 357.1709
-AICc(docmodel2) # 360.2143
-summary(docmodel2)
-
+DOC_nested <- glmer(Conc_ppm ~ Date/Reach + (1|Site) + (1|Replicate),
+                     data = DOC_data, 
+                     family = Gamma(link = "log"))
+Anova(DOC_nested)
+plot(DOC_nested)
+AIC(DOC_nested) # 398.9339
+AICc(DOC_nested) # 405.6785
 
 #### Post-hoc tests ####
 # Used in ribbon plot
-doc_emm_reach <- emmeans(docmodel1, ~ Reach|Date,
+# Reach by Date
+doc_reachdate_emm <- emmeans(docmodel1, ~ Reach|Date,
                        type = "response")
-doc_emm_reach_sum <- summary(doc_emm_reach)
+doc_reachdate_emm_sum <- summary(doc_reachdate_emm)
 
-##
-doc_emm_date <- emmeans(docmodel1, ~ Date|Reach,
+# Date by Reach
+doc_datereach_emm <- emmeans(docmodel1, ~ Date|Reach,
+                             type = "response")
+doc_datereach_emm_sum <- summary(doc_datereach_emm)
+
+## Reach 
+doc_reach_emm <- emmeans(docmodel1, ~ Reach,
                    type = "response")
-doc_emm_date_sum <- summary(doc_emm_date)
-
-##
-# I think the regrid backtransforms the model, it wouldn't work otherwise.
-# Used in boxplot
-doc_emm_reachtest <- regrid(emmeans(docmodel1, ~ Reach|Reach,
-                         type = "response"))
-doc_emm_reachtest_sum <- summary(doc_emm_reachtest)
+doc_reach_emm_sum <- summary(doc_reach_emm)
 
 ### CLD
-# Reach CLDs
-doc_reach_cld <- cld(doc_emm_reach,
-                 by = "Date",
+# Reach CLD
+doc_reach_cld <- cld(doc_reach_emm,
                  alpha = 0.05, 
                  Letters = letters,
                  decreasing = TRUE)
 doc_reach_cld$.group = gsub(" ", "", doc_reach_cld$.group)
-doc_reach_cld <- arrange(doc_reach_cld, Reach, Date)
-
-# Reach by Reach
-doc_reach_cldtest <- cld(doc_emm_reachtest,
-                     by = "Reach",
-                     alpha = 0.05, 
-                     Letters = letters,
-                     decreasing = TRUE)
-doc_reach_cldtest$.group = gsub(" ", "", doc_reach_cldtest$.group)
-doc_reach_cldtest <- arrange(doc_reach_cldtest, Reach)
-
-# Date CLD
-doc_date_cld <- cld(doc_emm_date,
-                     by = "Reach",
-                     alpha = 0.05, 
-                     Letters = letters,
-                     decreasing = TRUE)
-doc_date_cld$.group = gsub(" ", "", doc_date_cld$.group)
-doc_date_cld <- arrange(doc_date_cld, Reach, Date)
+doc_reach_cld <- arrange(doc_reach_cld, Reach)
 
 #### DOC Box Plots ####
 comparisons <- list(c("BDA", "REF"))
+
 ggplot(data = DOC_data, aes(x = Reach, y = Conc_ppm)) +
   geom_boxplot(aes(fill = Reach)) +
-  geom_point(data = doc_reach_cldtest, aes(x = Reach, y = response), size = 1, shape = 19,
+  geom_point(data = doc_reach_cld, aes(x = Reach, y = response), size = 1, shape = 19,
              color = "blue") +
-  geom_text(data = doc_reach_cldtest, aes(x = Reach, y = response, label= .group,
+  geom_text(data = doc_reach_cld, aes(x = Reach, y = response, label= .group,
                                   vjust = -2.2, hjust = 0.5),
             size = 5, position = position_dodge(0.5), color = "black") +
   scale_fill_manual(name = "Reach", labels = c("BDA", "Reference"), values = c("#3399FF", "#CC99FF")) +
@@ -174,43 +155,15 @@ ggplot(data = DOC_data, aes(x = Reach, y = Conc_ppm)) +
         axis.text = element_text(colour = "black", size = 12),
         # axis.text.x = element_blank(),
         axis.ticks.x = element_blank(), 
-        legend.position = "none") +
+        legend.position = "none") + # Says don't plot the legend
   scale_x_discrete(labels = c("Treatment", "Reference")) 
 
   stat_compare_means(comparisons = comparisons)
   stat_compare_means(lacel ="p.signif", method = "t.test", paired = F)
 
 #### DOC Ribbon Plot ####
-# Template plot
-ggplot(data = doc_emm_reach_sum) +
-  geom_ribbon(aes(x = Date,
-                  ymin = asymp.LCL, 
-                  ymax = asymp.UCL,
-                  group = Reach,
-                  fill = Reach), 
-              alpha = 0.40, 
-              color = NA) + # opaqueness of the CI
-  # fill = "#3984ff") +
-geom_line(aes(x = Date, 
-              y = response, 
-              group = Reach, 
-              color = Reach), 
-          lwd = 1) +
-  scale_y_continuous(expand = c(0,0)) +
-  scale_x_discrete(expand = c(0,0)) +
-  scale_color_manual(name = "Reach", labels = c("BDA", "Reference"), values = c("Blue", "Purple")) +
-  scale_fill_manual(name = "Reach", labels = c("BDA", "Reference"), values = c("Blue", "Purple")) + 
-  labs(title = "Dissolved Organic Carbon Concentrations", 
-       x = "Date", 
-       y = expression(Concentration~(mg~C~L^-1))) +
-  theme_bw() +
-  theme(plot.title = element_text(hjust = 0.5), 
-        axis.text = element_text(colour = "black"), 
-        axis.text.x = element_text(angle = 45, vjust = 0.5)) +
-  facet_grid(vars(rows = Site)) 
-
 # Concentration by Date - fill = Reach
-ggplot(data = doc_emm_reach_sum) +
+ggplot(data = doc_reachdate_emm_sum) +
   geom_ribbon(aes(x = Date,
                   ymin = asymp.LCL, 
                   ymax = asymp.UCL,
@@ -303,6 +256,7 @@ hist(SPOC_data$SPOC)
 #           Site = as.factor(ordered(Site, levels = c("LP", "FH", "TP"))), 
 #          Reach = as.factor(ordered(Reach, levels = c("DS", "BDA", "REF"))))
 
+# Replicate = random
 spocmodel <- glmer(SPOC ~ Date + Reach + (1|Site) + (1|Replicate),
                    data = SPOC_data, 
                    family = Gamma(link = "log"))
@@ -312,6 +266,7 @@ AIC(spocmodel) # -58.65812
 AICc(spocmodel) # -56.30098
 summary(spocmodel)
 
+# Replicate = NULL
 # This is the same structure as the DOC model 
 spocmodel1 <- glmer(SPOC ~ Date + Reach + (1|Site),
                    data = SPOC_data, 
@@ -322,56 +277,39 @@ AIC(spocmodel1) # -57.46977
 AICc(spocmodel1) # -55.52287
 summary(spocmodel1)
 
-# Response: SPOC
-# Chisq Df Pr(>Chisq)    
-# Date  62.628  6  1.314e-11 ***
-# Reach 13.036  1  0.0003055 ***
 
-SPOC_nested <- glmer(SPOC ~ Date/Site/Reach + (1|Replicate),
+SPOC_nested <- glmer(SPOC ~ Date/Reach + (1|Site) + (1|Replicate),
                               data = SPOC_data, 
                               family = Gamma(link = "log"))
 Anova(SPOC_nested)
 plot(SPOC_nested)
-AIC(SPOC_nested) # -100.7296
-AICc(SPOC_nested) # -49.301
+AIC(SPOC_nested) # -75.63007
+AICc(SPOC_nested) # -69.85649
 summary(SPOC_nested)
-# Response: SPOC
-# Chisq Df Pr(>Chisq)    
-# Date            155.04  6  < 2.2e-16 ***
-# Date:Site       472.28 14  < 2.2e-16 ***
-# Date:Site:Reach 241.90 21  < 2.2e-16 ***
+
 
 #### Post-hoc Tests ####
-# Nested Model
 # For Ribbon plot
-spoc_nestedreach_emm <- emmeans(SPOC_nested, ~ Reach|Site|Date,
+spoc_reachdate_emm <- emmeans(spocmodel1, ~ Reach|Date,
                    type = "response")
 
-spoc_nestedreach_emm_sum <- summary(spoc_nestedreach_emm)
+spoc_reachdate_emm_sum <- summary(spoc_reachdate_emm)
 
-# Non-nested model
-# For Ribbon plot
-spoc_reach_emm <- emmeans(spocmodel, ~ Reach|Date,
+# For Boxplot
+spoc_reach_emm <- emmeans(spocmodel1, ~ Reach,
                                 type = "response")
 
-spoc_reach_emm_sum <- summary(spoc_reach_emm)
-
-# For boxplot
-spoc_reach_boxemm <- regrid(emmeans(spocmodel, ~ Reach|Reach,
-                             type = "response"))
-
 ### CLD
-spoc_reach_boxemm_cld <- cld(spoc_reach_boxemm,
-                     by = "Reach",
+spoc_reach_emm_cld <- cld(spoc_reach_emm,
                      alpha = 0.05, 
                      Letters = letters,
                      decreasing = TRUE)
-spoc_reach_boxemm_cld$.group = gsub(" ", "", spoc_reach_boxemm_cld$.group)
-spoc_reach_boxemm_cld <- arrange(spoc_reach_boxemm_cld, Reach)
+spoc_reach_emm_cld$.group = gsub(" ", "", spoc_reach_emm_cld$.group)
+spoc_reach_emm_cld <- arrange(spoc_reach_emm_cld, Reach)
 
 #### SPOC Ribbon Plot ####
 # Nested model
-ggplot(data = spoc_nestedreach_emm_sum) +
+ggplot(data = spoc_reachdate_emm_sum) +
   geom_ribbon(aes(x = Date,
                   ymin = asymp.LCL, 
                   ymax = asymp.UCL,
@@ -395,50 +333,21 @@ ggplot(data = spoc_nestedreach_emm_sum) +
   theme_bw() +
   theme(plot.title = element_text(hjust = 0.5), 
         axis.text = element_text(colour = "black"), 
-        axis.text.x = element_text(angle = 45, vjust = 0.5)) +
-  facet_grid(vars(rows = Site))
-
-
-# Un-nested model
-ggplot(data = spoc_reach_emm_sum) +
-  geom_ribbon(aes(x = Date,
-                  ymin = asymp.LCL, 
-                  ymax = asymp.UCL,
-                  group = Reach,
-                  fill = Reach), 
-              alpha = 0.40, 
-              color = NA) + # opaqueness of the CI
-  # fill = "#3984ff") +
-  geom_line(aes(x = Date, 
-                y = response, 
-                group = Reach, 
-                color = Reach), 
-            lwd = 1) +
-  scale_y_continuous(expand = c(0,0)) +
-  scale_x_discrete(expand = c(0,0)) +
-  scale_color_manual(name = "Reach", labels = c("BDA", "Reference"), values = c("Blue", "Purple")) +
-  scale_fill_manual(name = "Reach", labels = c("BDA", "Reference"), values = c("Blue", "Purple")) + 
-  labs(title = "Particulate Organic Carbon Concentrations", 
-       x = "Date", 
-       y = expression(Concentration~(mg~C~L^-1))) +
-  theme_bw() +
-  theme(plot.title = element_text(hjust = 0.5), 
-        axis.text = element_text(colour = "black"), 
-        axis.text.x = element_text(angle = 45, vjust = 0.5)) 
+        axis.text.x = element_text(angle = 45, vjust = 0.5))
 
 #### SPOC Boxplot ####
 ggplot(data = SPOC_data, aes(x = Reach, y = SPOC)) +
   geom_boxplot(aes(fill = Reach)) +
-  geom_point(data = spoc_reach_boxemm_cld, aes(x = Reach, y = response), size = 1, shape = 19,
+  geom_point(data = spoc_reach_emm_cld, aes(x = Reach, y = response), size = 1, shape = 19,
              color = "blue") +
-  geom_text(data = spoc_reach_boxemm_cld, aes(x = Reach, y = response, label= .group,
+  geom_text(data = spoc_reach_emm_cld, aes(x = Reach, y = response, label= .group,
                                           vjust = -2.2, hjust = 0.5),
             size = 5, position = position_dodge(0.5), color = "black") +
   scale_fill_manual(name = "Reach", labels = c("BDA", "Reference"), values = c("#3399FF", "#CC99FF")) +
   # scale_fill_brewer(palette = "Spectral") +
-  labs(title = "Dissolved Organic Carbon Concentrations", 
+  labs(title = "Suspended Organic Carbon Concentrations", 
        x = NULL,
-       y = expression(DOC~(g~C~ml^-1))) +
+       y = expression(SPOC~(g~C~ml^-1))) +
   theme_bw() + 
   theme(plot.title = element_text(hjust = 0.5), 
         axis.text = element_text(colour = "black", size = 12),
@@ -446,7 +355,6 @@ ggplot(data = SPOC_data, aes(x = Reach, y = SPOC)) +
         axis.ticks.x = element_blank(), 
         legend.position = "none") +
   scale_x_discrete(labels = c("Treatment", "Reference"))
-
 
 #### DOC SPOC Ratios ####
 DOC <- DOC_data %>%
