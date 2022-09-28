e56a79dee196747cccf371b640299ae043ad0865
Hilary Schultz <hilary1.schultz@umconnect.umt.edu>
2022-09-28 15:21:48 -0600

    Cleaned and cleand plots

--- BDA_2021_SPOC_DOC.R
+++ BDA_2021_SPOC_DOC.R
@@ -14,27 +14,28 @@ library(lubridate)
 library(rstatix)
 library(MuMIn)
 library(gridExtra)
+library(cowplot)
 
 #### DOC #### 
-DOC <- read.csv("DOC_Data.csv", header = T, sep = ",") %>%
+DOC_data <- read.csv("DOC_Data.csv", header = T, sep = ",") %>%
   filter(Conc_ppm > 0, Reach != "GS", Reach != "DS") %>%
   select(Date, Site, Reach, Replicate, Conc_ppm) %>%
-  rename(Segment = Reach)
+  rename(Segment = Reach, Time = Date) 
 
+#### Outlier removal ####
 # Identifying outliers. The function identify_outliers() creates two new columns that ID outliers (1.5)
 # and extreme outliers (3), Which are 1.5 and 3 standard deviations away from the mean, respectively. 
 # I removed extreme outliers and doubled checked that those matched up with the observational data. 
-
-doc_outliers <- DOC %>%
+# Outlier removal due to obvious contaimination
+doc_outliers <- DOC_data %>%
   rstatix::identify_outliers("Conc_ppm") %>%
   filter(is.extreme == TRUE)
-
-# Removed outliers from dataframe
-DOC_data <- anti_join(DOC, doc_outliers) %>%
-  rename(Time = Date) 
-
+  
+DOC_data <- anti_join(DOC_data, doc_outliers)
+  
 # Setting this so the sampling dates are all at the start of the week
 DOC_data <- DOC_data %>%
+  mutate(Segment = if_else(Segment == "REF", "Reference", "Treatment")) %>%
   mutate(Time = if_else(Time == "07/14/2021", "07/15/2021", Time)) %>% # Date was off 
   mutate(Date = case_when(Time == "05/31/2021" ~ "06/01/2021",
                           Time == "06/02/2021" ~ "06/01/2021",
@@ -83,28 +84,22 @@ ggplot(aes(Conc_ppm)) +
 #### DOC GLMM ####
 finaldocmodel <- glmer(Conc_ppm ~ Date/Site/Segment + (1|Replicate),
                   data = DOC_data, 
+                  control = glmerControl(optimizer = "bobyqa",
+                                         optCtrl = list(maxfun = 100000)),
                   family = Gamma(link = "log"))
+
+doc_norm <- lmer(Conc_ppm ~ Date/Site/Segment + (1|Replicate),
+                 data = DOC_data)
+
 plot(finaldocmodel)
 Anova(finaldocmodel)
-AIC(finaldocmodel) # 360.3364 
-AICc(finaldocmodel) # 422.924 
-summary(finaldocmodel)
+
+qqnorm(resid(finaldocmodel))
+qqline(resid(finaldocmodel))
 
 # R2m = marginal - the variance explained by the fixed effects
 # R2c = conditional - the variance explained by the entire model, including random and fixed
 doc_fit <- r.squaredGLMM(finaldocmodel)
-
-# Seeing if the data meets the normal distribution assumption
-# shapiro.test(log(DOC_data$Conc_ppm)) # W = 0.85684, p-value = 0.00000000003756
-# qqPlot(log(DOC_data$Conc_ppm)) # This tests/shows if the data is normally distributed
-# small p-value implies that the distribution of the data is drastically special
-# from the normal distribution so we don't count on normality. 
-
-# It is not so we need to use the Levene test to see if there are differences
-# between the tested sample variances. 
-# levenetestdoc <- leveneTest(Conc_ppm ~ Date*Site*Segment, 
-                            # data = DOC_data)
-# p<0.05 says that the variances are relatively equal so we can use Tukey as post-hoc
 
 #### Post-hoc tests ####
 ### Emmeans
@@ -117,18 +112,6 @@ doc_emm_sum <- summary(doc_emm)
 doc_emmeans <- summary(doc_emm$emmeans)
 doc_emm_contrast <- summary(doc_emm$contrasts) # p values
 
-# doc_emm_contrast1 <- doc_emm_contrast %>%
-#   mutate(p.bon = p.adjust(p.value, method = "bonferroni"),
-#          p.holm = p.adjust(p.value, method = "holm"),
-#          p.sig = if_else(p.holm < signif(0.05,5), "*", ""))
-# doc_emm <- emmeans(finaldocmodel, ~ Reach|Site|Date,
-#                        type = "response",
-#                    nesting = "Date %in% Site, Reach %in% (Site*Date)")
-# doc_emm_sum <- summary(doc_emm)
-# doc_pair_sum$p.value <- p.adjust(doc_pair_sum$p.value, 
-#                              method = "bonferroni")
-# doc_p_adjust <- pairwise.t.test(doc_pair_sum$p.value, doc_pair_sum$contrast, p.adjust.method="bonferroni")
-
 ### CLD 
 doc_cld <- cld(doc_emm,
                          by = c("Site", "Date"),
@@ -140,7 +123,7 @@ doc_cld <- arrange(doc_cld, Date, Site, Segment)
 doc_cld$.group <- if_else(doc_cld$.group == "b", "*","")
 
 #### DOC Ribbon Plot ####
-ggplot(data = doc_emmeans) +
+doc_plot <-ggplot(data = doc_emmeans) +
   geom_ribbon(aes(x = Date,
                   ymin = asymp.LCL, 
                   ymax = asymp.UCL,
@@ -158,14 +141,15 @@ ggplot(data = doc_emmeans) +
   geom_text(data = doc_cld, aes(x = Date, y = response, label= .group,
                                  vjust = -1.5, hjust = 0.5),
             size = 6, position = position_dodge(0.5), color = "black") +
-  scale_y_continuous(expand = c(0,0)) +
+  ylim(0,10) +
+  # scale_y_continuous(expand = c(0,0)) +
   scale_x_discrete(guide = guide_axis(angle = 45), 
                    # labels = c("June 1, 2021", "", "June 14,2021", "", "July 12, 2021", "",
                    #            "August 9, 2021", "", "September 7, 2021"),
                    # labels = c("June","June","June", "June", "July","July", "August", "August", "September"),
                    expand = c(0,0)) +
-  scale_color_manual(name = "Segment", labels = c("Treatment", "Reference"), values = c("#0072B2", "#009E73")) +
-  scale_fill_manual(name = "Segment", labels = c("Treatment", "Reference"), values = c("#0072B2", "#009E73")) + 
+  scale_color_manual(values = c("#0072B2", "#009E73")) +
+  scale_fill_manual(values = c("#0072B2", "#009E73")) +
   labs(title = NULL, 
        x = NULL, 
        y = expression(Dissolved~Organic~Carbon~(mg~L^-1))) +
@@ -177,6 +161,8 @@ ggplot(data = doc_emmeans) +
         axis.title.y = element_text(size = 14)) +
   # axis.text.x = element_text(angle = 45, vjust = 0.5, hjust = 0.5)) +
   facet_grid(rows = vars(Site))
+
+save_plot("docfile.png", doc_plot, base_height = 6, base_width = 9)
 
 #### Stats ####
 doc_date_sum <- doc_emmeans %>%
@@ -203,19 +189,17 @@ std_error <- function(x) sd(x)/sqrt(length(x))
 SPOC_data <- read.csv("BDA_SPOC_Calc.csv", header = T, sep = ",") %>% 
   filter(SPOC > 0, Reach != "GS", Reach != "DS") %>%
   select(-Site.ID, -Sample, -Location) %>%
-  rename(Time = Date, Segment = Reach)
+  rename(Time = Date, Segment = Reach) 
 
 spoc_outliers <- SPOC_data %>%
   rstatix::identify_outliers("SPOC") %>%
-  filter(is.extreme == TRUE)
-
-SPOC_data <- anti_join(SPOC_data, spoc_outliers)
-
-# SPOC_summary <- SPOC_data %>% summarySE(groupvars = c("Site", "Date", "Reach"), measurevar = "SPOC") %>%
-#     rename(stdev = sd, stderror = se)
-# names(SPOC_summary)
+  filter(is.extreme == T)
 
 SPOC_data <- SPOC_data %>%
+  anti_join(spoc_outliers)
+
+SPOC_data <- SPOC_data %>%
+  mutate(Segment = if_else(Segment == "REF", "Reference", "Treatment")) %>%
   mutate(Time = if_else(Time == "8/9/2021", "08/09/2021", Time),
          Time = if_else(Time == "8/10/2021", "08/10/2021", Time), 
          Time = if_else(Time == "8/10/2021", "08/10/2021", Time),
@@ -256,16 +240,9 @@ SPOC_data$Segment <- as.factor(SPOC_data$Segment)
 SPOC_data$Site <- as.factor(SPOC_data$Site)
 SPOC_data$Replicate <- as.factor(SPOC_data$Replicate)
 
-
-# SPOC_data[,1:5] <- lapply(SPOC_data[,1:5], as.factor)
-
-# SPOC_data$Date <- ordered(SPOC_data$Date,
-#                          levels = c("6/14/2021", "6/28/2021","7/12/2021", "7/26/2021",
-#                                     "8/9/2021", "8/25/2021", "9/7/2021"))
-
 hist(SPOC_data$SPOC)
 
-write.csv(SPOC_data, "SPOC_data.csv", row.names = F)
+# write.csv(SPOC_data, "SPOC_data.csv", row.names = F)
 
 #### GLMM for SPOC ####
 # SPOC_data <- SPOC_data %>% 
@@ -273,7 +250,10 @@ write.csv(SPOC_data, "SPOC_data.csv", row.names = F)
 
 finalspocmodel <- glmer(SPOC ~ Date/Site/Segment + (1|Replicate),
                    data = SPOC_data, 
+                   control = glmerControl(optimizer = "bobyqa",
+                                          optCtrl = list(maxfun = 100000)),
                    family = Gamma(link = "log"))
+
 plot(finalspocmodel)
 Anova(finalspocmodel)
 AIC(finalspocmodel) # -129.5816
@@ -282,25 +262,7 @@ summary(finalspocmodel)
 
 spoc_fit <- r.squaredGLMM(finalspocmodel)
 
-# shapiro.test(SPOC_data)
-# shapiro.test(SPOC_data$SPOC) # W = 0.96985, p-value = 0.007091
-# qqPlot(SPOC_data$SPOC)
-# Not normally distributed 
-
-# levene_spoc <- SPOC_data %>%
-#   group_by(Date, Site) %>%
-#   levene_test(SPOC~Reach)
-# 
-# levenetestspoc <- leveneTest(SPOC ~ Date/Site/Reach, 
-#                             data = SPOC_data)
-# 
-# pwc <- SPOC_data %>%
-#   group_by(Date, Site) %>%
-#   pairwise_t_test(
-#     SPOC ~ Reach, paired = FALSE,
-#     p.adjust.method = "bonferroni"
-#   ) 
-
+#### Post-hoc Test
 spoc_emm <- emmeans(finalspocmodel, pairwise ~ Segment|Date|Site, 
                      type = "response",
                      nesting = "Date %in% Site, Segment %in% (Site*Date)")
@@ -308,22 +270,6 @@ spoc_emm_sum <- summary(spoc_emm)
 spoc_emmeans <- summary(spoc_emm$emmeans)
 spoc_emm_contrast <- summary(spoc_emm$contrasts) # p values
 
-# spoc_emm_contrast <- spoc_emm_contrast %>%
-#   mutate(p.bon = p.adjust(p.value, method = "bonferroni"),
-#          p.holm = p.adjust(p.value, method = "holm"),
-#          p.sig = if_else(p.value < signif(0.05,5), "*", ""))
-# 
-# spoc_emm1.1 <- spoc_emm$contrasts %>%
-#   rbind() %>%
-#   summary(infer = TRUE) %>%
-#   p.sig = if_else(p.value <= 0.05, "*", "")
-# 
-# contrast_df <- spoc_emm$contrasts %>%
-#   rbind() %>%
-#   as.data.frame() %>%
-#   mutate(p.sig = if_else(p.value <= 0.05, "*", ""))
-
-#### Post-hoc Tests ####
 ### CLD
 spoc_cld <- cld(spoc_emm,
                by = c("Site", "Date"),
@@ -336,7 +282,7 @@ spoc_cld <- arrange(spoc_cld, Date, Site, Segment)
 spoc_cld$.group <- if_else(spoc_cld$.group == "b", "*","")
 
 #### SPOC Ribbon Plot ####
-ggplot(data = spoc_emmeans) +
+spoc_plot <- ggplot(data = spoc_emmeans) +
   geom_ribbon(aes(x = Date,
                   ymin = asymp.LCL, 
                   ymax = asymp.UCL,
@@ -360,8 +306,8 @@ ggplot(data = spoc_emmeans) +
   #                             "August 9, 2021", "", "September 7, 2021"),
                    # labels = c("June","June","June", "July","July", "August", "August", "September"),
                    expand = c(0,0)) +
-  scale_color_manual(name = "Segment", labels = c("Treatment", "Reference"), values = c("#0072B2", "#009E73")) +
-  scale_fill_manual(name = "Segment", labels = c("Treatment", "Reference"), values = c("#0072B2", "#009E73")) + 
+  scale_color_manual(values = c("#0072B2", "#009E73")) +
+  scale_fill_manual(values = c("#0072B2", "#009E73")) + 
   labs(title = NULL, 
        x = NULL, 
        y = expression(Suspended~Particulate~Organic~Carbon~(mg~L^-1))) +
@@ -373,6 +319,8 @@ ggplot(data = spoc_emmeans) +
         axis.title.y = element_text(size = 14)) +
   # axis.text.x = element_text(angle = 45, vjust = 0.5, hjust = 0.5)) +
   facet_grid(rows = vars(Site))
+
+save_plot("spoc_plot.png", spoc_plot, base_height =6, base_width = 9)
 
 #### Stats ####
 emm_avg <- spoc_emmeans %>%
